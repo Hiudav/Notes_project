4dca89174af608241263dc8f1ffc4f8776e20eed
Hiudav <t0sha16@yandex.ru>
2024-08-08 15:22:06 +0300

    инициализация

---
+++ logger.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import json
+
+def Notes_create():
+    pass
+
+def Notes_between_date():
+    pass
+
+def Notes_read():
+    pass
+
+def Notes_edit():
+    pass
+
+def Notes_remove():
+    pass
